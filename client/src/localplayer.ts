58c7c54337f061c61dbc2f2d045ca0eb566e80cf
Temm <samumert@gmail.com>
2021-12-26 22:14:31 +0100

    Improve player movement code

--- client/src/localplayer.ts
+++ client/src/localplayer.ts
@@ -16,22 +16,16 @@ export class LocalPlayer extends Player {
     // forward / backward to move, left / right to rotate
     move(delta: number) {
         if (keyStates[this.controls.FORWARD]) {
-            let vec = new Vector3()
-            this.mesh.getWorldDirection(vec);
-            vec.setLength(this.speed / 100 * delta)
-            this.mesh.position.sub(vec);
+            this.mesh.translateZ(-this.speed * delta * 0.01);
         }
         if (keyStates[this.controls.BACKWARD]) {
-            let vec = new Vector3()
-            this.mesh.getWorldDirection(vec);
-            vec.setLength(this.speed / 100 * delta)
-            this.mesh.position.add(vec);
+            this.mesh.translateZ(this.speed * delta * 0.01);
         }
         if (keyStates[this.controls.LEFT]) {
-            this.mesh.rotation.y += 0.002 * delta;
+            this.mesh.rotateY(0.002 * delta);
         }
         if (keyStates[this.controls.RIGHT]) {
-            this.mesh.rotation.y -= 0.002 * delta;
+            this.mesh.rotateY(-0.002 * delta);
         }
     }
     shoot() {
